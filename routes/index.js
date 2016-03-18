85bb5479ca4e843b5c55e88d4fc8ec9d54812192
Ubuntu <mauraisaqua@yahoo.com>
2016-03-18 21:30:06 +0000

    added user path to routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/user', function(req, res, next) {
+ 	User.find(function(err, users) {
+	 	res.json(users);
+	});
 });
 
 module.exports = router;
